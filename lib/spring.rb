e3a9963700637f4ef9a0a6aee5f52aa0230dd077
dlozano <dlozano@users.noreply.github.com>
2024-01-19 12:39:51 +0100

    Foo

---
+++ lib/spring.rb
@@ -0,0 +1,25 @@
+module Spring
+  class << self
+    def unbundled_env
+      env = original_env
+
+      if env.key?("BUNDLER_ORIG_MANPATH")
+        env["MANPATH"] = env["BUNDLER_ORIG_MANPATH"]
+      end
+
+      env.delete_if {|k, _| k[0, 7] == "BUNDLE_" }
+
+      if env.key?("RUBYOPT")
+        env["RUBYOPT"] = env["RUBYOPT"].sub "-rbundler/setup", ""
+      end
+
+      if env.key?("RUBYLIB")
+        rubylib = env["RUBYLIB"].split(File::PATH_SEPARATOR)
+        rubylib.delete(File.expand_path("..", __FILE__))
+        env["RUBYLIB"] = rubylib.join(File::PATH_SEPARATOR)
+      end
+
+      env
+    end
+  end
+end
